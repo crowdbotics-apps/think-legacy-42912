7137d9e20db19a99350c7d8af842ef94ff1ca335
Crowdbotics <team@crowdbotics.com>
2023-08-17 21:59:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "think-legacy-42912",
+  url: "https://think-legacy-42912.botics.co",
+  api: "https://think-legacy-42912.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
